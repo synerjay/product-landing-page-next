5403d598a414fe1b3449c1632e6cd873ef73bc3c
Jerome Tolentino <tolent.jay@gmail.com>
2021-10-08 18:25:44 +0900

    added navbar

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import Header from '../components/Header';
+import Layout from '../components/Layout';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div>
-      <h2>Welcome to my homepage</h2>
-    </div>
+    <Layout pageTitle='Landing Page Nextjs'>
+      <Header />
+    </Layout>
   );
 }
