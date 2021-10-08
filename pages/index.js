cba8451ce9891b80b8dfea509dd0d069c15c12f6
Jerome Tolentino <tolent.jay@gmail.com>
2021-10-08 18:43:34 +0900

    added components

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import About from '../components/About';
+import Feature from '../components/Feature';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 import Layout from '../components/Layout';
+import Service from '../components/Service';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
@@ -10,6 +14,10 @@ export default function Home() {
     <Layout pageTitle='Landing Page Nextjs'>
       <Header />
       <Hero />
+      <Feature />
+      <Service />
+      <About />
+      <Footer />
     </Layout>
   );
 }
